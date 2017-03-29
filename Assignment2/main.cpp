6627ec2c4d59db76c0c1e251f09cee65a625c09a
pleycpl <h4rvey@protonmail.com>
2017-03-30 00:46:40 +0300

    more adding user, and testing contains function

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -10,9 +10,18 @@ int main() {
   root = insertNewUserWithFriends(root, 20, "Ozgur", "Yildirim", 2, 4, 5);
   root = insertNewUserWithFriends(root, 30, "Esra", "Atas", 1, 3);
   root = insertNewUserWithFriends(root, 40, "Emre", "Bugra", 1, 6);
-  root = insertNewUserWithFriends(root, 50, "Okan", "Yeah", 0);
+  root = insertNewUserWithFriends(root, 10, "Okan", "Yeah", 0);
+  root = insertNewUserWithFriends(root, 15, "Esra", "Atas", 3, 1, 2, 3);
+  root = insertNewUserWithFriends(root, 35, "Emre", "Bugra", 4, 10, 20, 30, 15);
+  root = insertNewUserWithFriends(root, 42, "Okan", "Yeah", 0);
+  root = insertNewUserWithFriends(root, 5, "Esra", "Atas", 1, 10);
+  root = insertNewUserWithFriends(root, 28, "Emre", "Bugra", 2, 80, 90);
+  root = insertNewUserWithFriends(root, 80, "Okan", "Yeah", 2, 100, 110);
   displayTreeWithInorder(root);
 
+  cout << endl << "[=] Testing Contains Function" << endl;
+  bool status = contains(root, 10);
+  cout << "Status: " << status << endl;
   destroyFriends(root);
   destroyNodes(root);
   return 0;
