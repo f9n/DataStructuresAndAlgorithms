68a282f5064fdf60d3e2acbca62464bf8cfabeca
pleycpl <h4rvey@protonmail.com>
2017-03-30 00:07:07 +0300

    added function prototypes

--- Assignment2/util.h
+++ Assignment2/util.h
@@ -2,6 +2,7 @@
 #define UTIL_H
 #include <stdlib.h>
 #include <string>
+#include <stdarg.h>
 using namespace std;
 struct n {
   int IdNo;
@@ -15,8 +16,10 @@ struct n {
 typedef struct n node;
 
 void displayOneNode(node * root);
-void displayFriendOneNode(node * root);
-node * insertNewUser(node * root, int x, string username, string surname);
+void displayFriendsOneNode(node * root);
+node * insertNewUserWithFriends(node * root, int x, string username, string surname, int args, ...);
+node * insertNewUser(node * root, int x, string username, string surname, int * IdOfFriends, int IdOfFriendsAmount);
 void displayTreeWithInorder(node * tree);
-
+void destroyFriends(node * tree);
+void destroyNodes(node * tree);
 #endif // UTIL_H
