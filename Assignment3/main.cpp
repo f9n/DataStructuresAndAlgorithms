0002d9687a7d7dc7cba7629ebf1f6f1ad9a75385
pleycpl <h4rvey@protonmail.com>
2017-05-06 13:45:03 +0000

    added displayStringArray function

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -2,15 +2,18 @@
 #include <fstream>
 #include <string>
 
+#define FILESIZE 2415
+
 using namespace std;
 
 int main() {
   string *stringArray = readStringsFromFile("kelime.txt");
-
+  displayStringArray(stringArray);
   free(stringArray);
   return 0;
 }
 string * readStringsFromFile(string filename) {
+  cout << "[+] Runned readStringsFromFile function" << endl;
   /* Allocate stringArray */
   string * stringArray = (string *)malloc(sizeof(string)*FILESIZE);
   /* Read File */
@@ -26,4 +29,9 @@ string * readStringsFromFile(string filename) {
     readFile.close();
   }
   return stringArray;
+}
+void displayStringArray(string * array) {
+  for(int i=0; i < FILESIZE; i++) {
+    cout << i << "  index: " + array[i] << endl;
+  }
 }
